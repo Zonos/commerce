1157657557c2e49946876529481701f80f5d9ca4
nick.lanners <nick.lanners@zonos.com>
2025-04-15 13:09:55 -0600

    fix: prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
-import { CartProvider } from 'components/cart/cart-context';
-import { Navbar } from 'components/layout/navbar';
-import { ZonosLayoutSetup } from 'components/zonos/ZonosLayoutSetup';
-import { GeistSans } from 'geist/font/sans';
-import { baseUrl } from 'lib/utils';
-import { getCart } from 'lib/zonos';
-import Script from 'next/script';
-import { ReactNode, useMemo } from 'react';
-import { Toaster } from 'sonner';
-import './globals.css';
+import { CartProvider } from "components/cart/cart-context";
+import { Navbar } from "components/layout/navbar";
+import { ZonosLayoutSetup } from "components/zonos/ZonosLayoutSetup";
+import { GeistSans } from "geist/font/sans";
+import { baseUrl } from "lib/utils";
+import { getCart } from "lib/zonos";
+import Script from "next/script";
+import { ReactNode, useMemo } from "react";
+import { Toaster } from "sonner";
+import "./globals.css";
 
 const { SITE_NAME } = process.env;
 
@@ -36,11 +36,11 @@ export default async function RootLayout({
   const cart = getCart();
 
   return (
-    <html lang='en' className={GeistSans.variable}>
-      <body className='bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white'>
+    <html lang="en" className={GeistSans.variable}>
+      <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
         <Script
           src={`${ZONOS_CDN_URL}/dist/scripts/loadZonos.js?timestamp=${timestamp}`}
-          strategy='beforeInteractive'
+          strategy="beforeInteractive"
         />
         <ZonosLayoutSetup>
           <CartProvider cartPromise={cart}>
--- components/zonos/ZonosLayoutSetup.tsx
+++ components/zonos/ZonosLayoutSetup.tsx
@@ -1,10 +1,10 @@
-'use client';
-import { type ReactNode, Suspense, useEffect } from 'react';
+"use client";
+import { type ReactNode, Suspense, useEffect } from "react";
 
 const ZonosLayoutSetupWrapper = ({ children }: { children: ReactNode }) => {
   useEffect(() => {
     const getCartId = async () => {
-      const yourServerUrl = '/api/zonos/get-cart-id';
+      const yourServerUrl = "/api/zonos/get-cart-id";
       const response = await fetch(yourServerUrl);
       const json = await response.json();
       return json;
@@ -18,21 +18,21 @@ const ZonosLayoutSetupWrapper = ({ children }: { children: ReactNode }) => {
     void window.Zonos.init({
       checkoutSettings: {
         createCartId: getCartId,
-        placeOrderButtonSelector: '#checkout-button',
+        placeOrderButtonSelector: "#checkout-button",
       },
       storeId: 7744, // Contact support for this information.
-      zonosApiKey: 'credential_live_7a128f4e-f192-4232-8992-94dd09eb4437', // Contact support for this information
+      zonosApiKey: "credential_live_7a128f4e-f192-4232-8992-94dd09eb4437", // Contact support for this information
       helloSettings: {
         onInitSuccess: async () => {
           window.Zonos.openHelloDialog();
         },
-        productAddToCartElementSelector: '.add-to-cart',
-        productDescriptionElementSelector: '.product-description',
-        productDetailUrlPattern: '/products/.*$',
-        productListUrlPattern: '/products',
-        productTitleElementSelector: '.product-title',
-        showForCountries: 'ALL',
-        currencyElementSelector: '.product-price',
+        productAddToCartElementSelector: ".add-to-cart",
+        productDescriptionElementSelector: ".product-description",
+        productDetailUrlPattern: "/products/.*$",
+        productListUrlPattern: "/products",
+        productTitleElementSelector: ".product-title",
+        showForCountries: "ALL",
+        currencyElementSelector: ".product-price",
       },
     });
   }, []);
